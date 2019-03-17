05e7d4cd14300262a97c1b439d6868af543537e3
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2019-03-17 11:58:41 -0400

    ENH: Add tests to the module.
    Add tests to the module.

---
+++ test/itkTimeVaryingVelocityFieldSemiLagrangianIntegrationImageFilterTest.cxx
@@ -0,0 +1,58 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkTimeVaryingVelocityFieldSemiLagrangianIntegrationImageFilter.h"
+#include "itkImageFileWriter.h"
+#include "itkTestingMacros.h"
+
+
+int itkTimeVaryingVelocityFieldSemiLagrangianIntegrationImageFilterTest( int argc, char * argv[] )
+{
+  if( argc < 2 )
+    {
+    std::cerr << "Missing parameters." << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " outputImage";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  constexpr unsigned int Dimension = 3;
+
+  using PixelType = float;
+  using ImageType = itk::Image< PixelType, Dimension >;
+
+  using VectorType = itk::Vector< PixelType, Dimension >;
+  using DisplacementFieldType = itk::Image< VectorType, Dimension >;
+  using TimeVaryingVelocityFieldType = itk::Image< VectorType, Dimension >;
+
+  using TimeVaryingVelocityFieldSemiLagrangianIntegrationImageFilterType =
+    itk::TimeVaryingVelocityFieldSemiLagrangianIntegrationImageFilter< TimeVaryingVelocityFieldType,
+    DisplacementFieldType >;
+  TimeVaryingVelocityFieldSemiLagrangianIntegrationImageFilterType::Pointer
+    timeVaryingVelocityFieldSemiLagrangianIntegrationImageFilter =
+    TimeVaryingVelocityFieldSemiLagrangianIntegrationImageFilterType::New();
+
+  EXERCISE_BASIC_OBJECT_METHODS( timeVaryingVelocityFieldSemiLagrangianIntegrationImageFilter,
+    TimeVaryingVelocityFieldSemiLagrangianIntegrationImageFilter, TimeVaryingVelocityFieldIntegrationImageFilter );
+
+
+
+  std::cout << "Test finished." << std::endl;
+  return EXIT_SUCCESS;
+}
---
+++ test/itkTimeVaryingVelocityFieldSemiLagrangianTransformTest.cxx
@@ -0,0 +1,52 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkTimeVaryingVelocityFieldSemiLagrangianTransform.h"
+#include "itkImageFileWriter.h"
+#include "itkTestingMacros.h"
+
+
+int itkTimeVaryingVelocityFieldSemiLagrangianTransformTest( int argc, char * argv[] )
+{
+  if( argc < 2 )
+    {
+    std::cerr << "Missing parameters." << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " outputImage";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  constexpr unsigned int Dimension = 2;
+
+  using PixelType = float;
+  using ImageType = itk::Image< PixelType, Dimension >;
+
+  using TimeVaryingVelocityFieldSemiLagrangianTransformType =
+    itk::TimeVaryingVelocityFieldSemiLagrangianTransform< PixelType >;
+  TimeVaryingVelocityFieldSemiLagrangianTransform::Pointer timeVaryingVelocityFieldSemiLagrangianTransform =
+    TimeVaryingVelocityFieldSemiLagrangianTransform::New();
+
+  EXERCISE_BASIC_OBJECT_METHODS( timeVaryingVelocityFieldSemiLagrangianTransform,
+    TimeVaryingVelocityFieldSemiLagrangianTransform, TimeVaryingVelocityFieldTransform );
+
+
+
+  std::cout << "Test finished." << std::endl;
+  return EXIT_SUCCESS;
+}
