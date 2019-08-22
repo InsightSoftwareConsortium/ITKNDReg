aafd81995af269dfc0aae9e0cdf2247b4af563a1
Matt McCormick <matt.mccormick@kitware.com>
2019-08-21 21:40:59 -0400

    ENH: Build Python package against ITK 5.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-ndreg',
-    version='0.1.0',
+    version='0.1.1',
     author='Neurodata',
     author_email='vikramc@jhmi.edu',
     packages=['itk'],
@@ -44,6 +44,6 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://itk.org/',
     install_requires=[
-        r'itk'
+        r'itk>=5.0.1'
     ]
     )
