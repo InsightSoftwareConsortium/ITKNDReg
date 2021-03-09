529efaefeeaeaebc28c05d8b1ff74843dab4d5e0
Matt McCormick <matt.mccormick@kitware.com>
2021-03-09 14:28:39 -0500

    ENH: Bump Python Package version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-ndreg',
-    version='0.2.0',
+    version='0.2.1',
     author='Neurodata',
     author_email='vikramc@jhmi.edu',
     packages=['itk'],
